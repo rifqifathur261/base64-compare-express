f7a3d138c4d7597fe1d2e3ff58da8b5ee13ca1ed
Rifqi Fathurrahman <goniawan261@gmail.com>
2022-12-21 15:04:31 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3000
+
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', (req, res) => {
+    res.send({ 'message': 'Compare Base64 API!' })
+})
+app.post('/compare', async (req, res) => {
+    const body = req.body
+    try {
+
+        const response = await axios.post('http://192.168.20.221:1800/insocio/compare-image', body)
+
+        res.send({ message: 'Success', data: response.data })
+    } catch (error) {
+        res.status(409).send({
+            message: error.message || "Some error while comparing image."
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${ port }`)
+})
